d83fcbaebfc99028914200b632c2b909971a96c5
uibnui <uibnui@gmail.com>
2018-09-12 21:12:13 +0300

    Add files via upload

---
+++ RuutuNoAdds.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         RuutuNoAdds
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       uibnui
+// @match        https://www.ruutu.fi/video/*
+// @grant        GM_openInTab
+// ==/UserScript==
+function videoID(url)
+{
+    console.log(url);
+    var pos = url.search("/video/");
+    console.log(pos);
+    return url.slice((pos+7));
+}
+var id = videoID(window.location.href);
+var xml = "https://gatling.ruutu.fi/media-xml-cache?id=" + id;
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", xml, false ); // false for synchronous request
+xmlHttp.send( null );
+var parser = new DOMParser();
+xmlDoc = parser.parseFromString(xmlHttp.response,"text/xml");
+var node = xmlDoc.getElementsByTagName('CastMediaFile')[0].childNodes[0].nodeValue;
+console.log(node);
+//Change comment to window.location if you want video to open in new tab
+//GM_openInTab(node, false, true, false);
+window.location.href = node;
